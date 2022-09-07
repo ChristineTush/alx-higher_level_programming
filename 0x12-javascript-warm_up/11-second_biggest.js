6366ae7cd27f172ad393db355574597bf7c22857
Christine Muthoka <61786722+ChristineTush@users.noreply.github.com>
2022-09-07 08:30:58 +0300

    new update task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
-// searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+let numbers = process.argv.slice(2);
+numbers = numbers.map(number => parseInt(number, 10));
+
+let biggest = 0;
+let secondBiggest = 0;
+
+for (const num of numbers) {
+  if (num >= biggest) {
+    secondBiggest = biggest;
+    biggest = num;
+  }
+  if (num < biggest && num >= secondBiggest) secondBiggest = num;
 }
+
+console.log(secondBiggest);
